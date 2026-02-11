2949f3d547759d4906a9a9ac8636a2987bb1c1bb
Marcela Herrera <mh460877@gmail.com>
2026-02-11 19:10:03 -0300

    Update constants.js

--- assets/js/config/constants.js
+++ assets/js/config/constants.js
@@ -1,2 +1,2 @@
-export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyatPOfadZNAH-_QKeqij8Q2Xlcn3ckmGktH_nFwLFaQd8bP7Cb-7DBYKDijQs9Mskm3g/exec';
+export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwOq4YY1wFM0SSBYPLKG7k30WBHgAZ60JY5YR3LHD31l7Eqx9CPGd2lnkrbsFs1jSYiJA/exec';
 export const DB_GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby6kBo-HW69iQ6sUx0YbU_xR2qsufgVLOkNCnpPBYeb0A_dn3_u3yR4Q45iK8v9FbdC/exec';
